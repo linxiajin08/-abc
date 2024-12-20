9147111787c0881fef932510cb70717499c890fb
linxiajin08 <1835377858@qq.com>
2024-12-20 08:59:53 +0800

    林夏槿编程abc
    林夏槿编程abc

---
+++ 未命名1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define a int
+#define b return 0
+#define c main()
+#define d if
+#define e else
+#define f printf
+#define g scanf
+#define h for
+#define i while
+#define j do
+#define k break
+a c
+{
+	
+	b;
+}
